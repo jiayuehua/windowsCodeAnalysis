953f309c6d61c078f029c2647c8d13c30032e43c
Jia Yue Hua <3423893+jiayuehua@users.noreply.github.com>
2023-07-17 01:23:19 +0800

    Update windowsCodeAnalysis.cpp

--- windowsCodeAnalysis.cpp
+++ windowsCodeAnalysis.cpp
@@ -13,9 +13,26 @@ int foo(int a, int b)
 struct S {
 	int i ;
 };
+string* bad()   // really bad
+{
+    vector<string> v = { "This", "will", "cause", "trouble", "!" };
+    // leaking a pointer into a destroyed member of a destroyed object (v)
+    return &v[0];
+}
+
+void use()
+{
+    string* p = bad();
+    vector<int> xx = {7, 8, 9};
+    // undefined behavior: x might not be the string "This"
+    string x = *p;
+    // undefined behavior: we don't know what (if anything) is allocated a location p
+    *p = "Evil!";
+}
 
 int main()
 {
+	use();
 	cout << "Hello CMake." << endl;
   foo(2,3);
 	S s;
