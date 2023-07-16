ecfbbc5eca85fa3a65fbb71b5d10521da0419f61
jiayuehua <3423893+jiayuehua@users.noreply.github.com>
2023-07-16 13:14:30 +0800

    Add project files.

---
+++ windowsCodeAnalysis.cpp
@@ -0,0 +1,18 @@
+﻿// windowsCodeAnalysis.cpp : Defines the entry point for the application.
+//
+
+#include "windowsCodeAnalysis.h"
+
+using namespace std;
+
+int foo(int a, int b)
+{
+	return 3;
+}
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+  foo(2,3);
+	return 0;
+}
